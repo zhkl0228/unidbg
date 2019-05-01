b541eb1b68f6b7c133610408b14a0e0eb474d4b1
Banny <zhkl0228@qq.com>
2019-05-01 13:12:58 +0800

    小优化

--- src/main/java/cn/banny/emulator/linux/AndroidElfLoader.java
+++ src/main/java/cn/banny/emulator/linux/AndroidElfLoader.java
@@ -1,7 +1,10 @@
 package cn.banny.emulator.linux;
 
 import cn.banny.auxiliary.Inspector;
-import cn.banny.emulator.*;
+import cn.banny.emulator.Alignment;
+import cn.banny.emulator.Emulator;
+import cn.banny.emulator.Module;
+import cn.banny.emulator.Symbol;
 import cn.banny.emulator.arm.ARMEmulator;
 import cn.banny.emulator.hook.HookListener;
 import cn.banny.emulator.linux.android.ElfLibraryFile;
@@ -129,7 +132,7 @@ public class AndroidElfLoader extends AbstractLoader implements Memory, Loader {
     @Override
     public byte[] unpack(File elfFile) throws IOException {
         final byte[] fileData = FileUtils.readFileToByteArray(elfFile);
-        loadInternal(new ElfLibraryFile(elfFile), new WriteHook() {
+        LinuxModule module = loadInternal(new ElfLibraryFile(elfFile), new WriteHook() {
             @Override
             public void hook(Unicorn u, long address, int size, long value, Object user) {
                 byte[] data = Arrays.copyOf(ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putLong(value).array(), size);
@@ -139,6 +142,7 @@ public class AndroidElfLoader extends AbstractLoader implements Memory, Loader {
                 System.arraycopy(data, 0, fileData, (int) address, data.length);
             }
         }, true);
+        emulator.createDalvikVM(null).callJNI_OnLoad(emulator, module);
         return fileData;
     }
 
