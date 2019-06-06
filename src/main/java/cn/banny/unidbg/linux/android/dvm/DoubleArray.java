02559da02e3a09c0ad56829ab6f09dd21b7d033e
Banny <zhkl0228@gmail.com>
2019-06-07 02:48:44 +0800

    Update DoubleArray.java

--- src/main/java/cn/banny/unidbg/linux/android/dvm/DoubleArray.java
+++ src/main/java/cn/banny/unidbg/linux/android/dvm/DoubleArray.java
@@ -1,22 +1,14 @@
 package cn.banny.unidbg.linux.android.dvm;
 
-import cn.banny.unidbg.memory.MemoryBlock;
-
 public class DoubleArray extends DvmObject<double[]> implements Array<double[]> {
 
     public DoubleArray(double[] value) {
         super(null, value);
     }
 
-    MemoryBlock memoryBlock;
-
     @Override
     public int length() {
         return value.length;
-    }
-
-    void setValue(double[] value) {
-        super.value = value;
     }
 
     @Override
