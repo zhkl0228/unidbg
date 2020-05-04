61174512fcc6851d0add578b59f60fd0552aa188
android <android@gmail.com>
2020-05-04 15:58:55 +0800

    iOS add more framework

--- src/main/native/ios/framework/build.sh
+++ src/main/native/ios/framework/build.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-xcrun -sdk iphoneos clang -o UIKit UIKit.m -shared -lobjc -m32 -arch armv7 -miphoneos-version-min=7.1 -framework Foundation && \
-mv UIKit UIKit32 && \
-xcrun -sdk iphoneos clang -o UIKit UIKit.m -shared -lobjc -m64 -arch arm64 -miphoneos-version-min=7.1 -framework Foundation && \
-mv UIKit UIKit64 && \
-lipo -create UIKit32 UIKit64 -output UIKit && \
-rm UIKit32 UIKit64 && \
-mv UIKit ../../../resources/ios/7.1/System/Library/Frameworks/UIKit.framework/
+function build_framework() {
+  xcrun -sdk iphoneos clang -o "$1"/"$1" "$1"/"$1".m -shared -lobjc -m32 -arch armv7 -miphoneos-version-min=7.1 -framework "$2" && \
+  mv "$1"/"$1" "$1"/"$1"32 && \
+  xcrun -sdk iphoneos clang -o "$1"/"$1" "$1"/"$1".m -shared -lobjc -m64 -arch arm64 -miphoneos-version-min=7.1 -framework "$2" && \
+  mv "$1"/"$1" "$1"/"$1"64 && \
+  lipo -create "$1"/"$1"32 "$1"/"$1"64 -output "$1"/"$1" && \
+  rm "$1"/"$1"32 "$1"/"$1"64 && \
+  mkdir -p ../../../resources/ios/7.1/System/Library/Frameworks/"$1".framework && \
+  mv "$1"/"$1" ../../../resources/ios/7.1/System/Library/Frameworks/"$1".framework/
+}
+
+build_framework UIKit "Foundation"
+build_framework JavaScriptCore "Foundation"
+build_framework MultipeerConnectivity "Foundation"
+build_framework PushKit "Foundation"
+build_framework WebKit "Foundation"
+build_framework AdSupport "Foundation"
+build_framework Photos "Foundation"
+build_framework ExternalAccessory "Foundation"
+build_framework AddressBookUI "Foundation"
+build_framework AddressBook "Foundation"
+build_framework CoreLocation "Foundation"
+build_framework ImageIO "Foundation"
+build_framework AVFoundation "Foundation"
+build_framework AudioToolbox "Foundation"
+build_framework CoreMotion "Foundation"
+build_framework GLKit "Foundation"
+build_framework MediaPlayer "Foundation"
+build_framework SceneKit "Foundation"
