0afbaf151e32607b5e11edb5febbad71acc9743b
Banny <zhkl0228@qq.com>
2020-09-24 19:35:03 +0800

    Update a12z tests

--- unidbg-ios/src/test/java/com/github/unidbg/ios/A12ZTest.java
+++ unidbg-ios/src/test/java/com/github/unidbg/ios/A12ZTest.java
@@ -15,11 +15,11 @@ public class A12ZTest {
     public static void main(String[] args) throws IOException {
         Logger.getLogger(Stdout.class).setLevel(Level.DEBUG);
         Logger.getLogger(AbstractEmulator.class).setLevel(Level.DEBUG);
-        Emulator<?> emulator = new DarwinARM64Emulator();
+        Emulator<?> emulator = new DarwinARMEmulator();
         emulator.getMemory().setLibraryResolver(new DarwinResolver());
         emulator.getSyscallHandler().setVerbose(true);
 
-        Module module = emulator.loadLibrary(new File("unidbg-ios/src/test/resources/example_binaries/a12z_osx"));
+        Module module = emulator.loadLibrary(new File("unidbg-ios/src/test/resources/example_binaries/a12z_ios"));
 
         emulator.attach().addBreakPoint(null, 0x0000000100003F40L);
         long start = System.currentTimeMillis();
