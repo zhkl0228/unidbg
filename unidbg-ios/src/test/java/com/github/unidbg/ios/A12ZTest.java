aa325b477de79d65867a922880f2fb431b4742a8
android <android@gmail.com>
2020-09-25 14:10:02 +0800

    Update a12z.m

--- unidbg-ios/src/test/java/com/github/unidbg/ios/A12ZTest.java
+++ unidbg-ios/src/test/java/com/github/unidbg/ios/A12ZTest.java
@@ -2,10 +2,7 @@ package com.github.unidbg.ios;
 
 import com.github.unidbg.Emulator;
 import com.github.unidbg.Module;
-import com.github.unidbg.Symbol;
 import com.github.unidbg.memory.Memory;
-import com.github.unidbg.pointer.UnicornPointer;
-import com.sun.jna.Pointer;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,17 +16,6 @@ public class A12ZTest {
         emulator.getSyscallHandler().setVerbose(true);
 
         Module module = emulator.loadLibrary(new File("unidbg-ios/src/test/resources/example_binaries/a12z_osx"));
-        Module cm = memory.findModule("libsystem_c.dylib");
-        Symbol setvbuf = cm.findSymbolByName("_setvbuf", false);
-        Symbol __stdoutp = cm.findSymbolByName("___stdoutp", false);
-        Symbol __stderrp = cm.findSymbolByName("___stderrp", false);
-        Pointer stdoutp = UnicornPointer.pointer(emulator, __stdoutp.getAddress());
-        Pointer stderrp = UnicornPointer.pointer(emulator, __stderrp.getAddress());
-        assert stdoutp != null && stderrp != null;
-        final int _IONBF = 2;
-        setvbuf.call(emulator, stdoutp.getPointer(0), null, _IONBF, 0);
-        setvbuf.call(emulator, stderrp.getPointer(0), null, _IONBF, 0);
-
         long start = System.currentTimeMillis();
         int ret = module.callEntry(emulator);
         System.err.println("testA12Z ret=0x" + Integer.toHexString(ret) + ", offset=" + (System.currentTimeMillis() - start) + "ms");
--- unidbg-ios/src/test/resources/native/macos/a12z.m
+++ unidbg-ios/src/test/resources/native/macos/a12z.m
@@ -2,6 +2,8 @@
 #include <Foundation/Foundation.h>
 
 int main(int argc, char *argv[]) {
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stderr, NULL, _IONBF, 0);
   NSLog(@"A12Z double=%lu, long double=%lu, long=%lu", sizeof(double), sizeof(long double), sizeof(long));
   printf("hello world A12Z, double=%lu, long double=%lu, long=%lu\n", sizeof(double), sizeof(long double), sizeof(long));
   return 0;
