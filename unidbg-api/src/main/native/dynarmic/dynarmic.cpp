41c6b25845b608ddb4896477b34b0c9bb467b882
android <android@gmail.com>
2020-10-09 15:17:48 +0800

    Add DynarmicCallbacks64

--- unidbg-api/src/main/native/dynarmic/dynarmic.cpp
+++ unidbg-api/src/main/native/dynarmic/dynarmic.cpp
@@ -3,14 +3,113 @@
 #include <cstdio>
 #include <exception>
 
-#include <dynarmic/A32/a32.h>
-#include <dynarmic/A32/config.h>
-
-#include <dynarmic/A64/a64.h>
-#include <dynarmic/A64/config.h>
-
 #include <sys/mman.h>
 #include "dynarmic.h"
+
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+class DynarmicCallbacks64 final : public Dynarmic::A64::UserCallbacks {
+public:
+    u8 MemoryRead8(u64 vaddr) override {
+        fprintf(stderr, "MemoryRead8[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return 0;
+    }
+    u16 MemoryRead16(u64 vaddr) override {
+        fprintf(stderr, "MemoryRead16[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return 0;
+    }
+    u32 MemoryRead32(u64 vaddr) override {
+        fprintf(stderr, "MemoryRead32[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return 0;
+    }
+    u64 MemoryRead64(u64 vaddr) override {
+        fprintf(stderr, "MemoryRead64[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return 0;
+    }
+    Dynarmic::A64::Vector MemoryRead128(u64 vaddr) override {
+        return {MemoryRead64(vaddr), MemoryRead64(vaddr + 8)};
+    }
+
+    void MemoryWrite8(u64 vaddr, u8 value) override {
+        fprintf(stderr, "MemoryWrite8[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+    }
+    void MemoryWrite16(u64 vaddr, u16 value) override {
+        fprintf(stderr, "MemoryWrite16[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+    }
+    void MemoryWrite32(u64 vaddr, u32 value) override {
+        fprintf(stderr, "MemoryWrite32[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+    }
+    void MemoryWrite64(u64 vaddr, u64 value) override {
+        fprintf(stderr, "MemoryWrite64[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+    }
+    void MemoryWrite128(u64 vaddr, Dynarmic::A64::Vector value) override {
+        MemoryWrite64(vaddr, value[0]);
+        MemoryWrite64(vaddr + 8, value[1]);
+    }
+
+    bool MemoryWriteExclusive8(u64 vaddr, std::uint8_t value, std::uint8_t expected) override {
+        fprintf(stderr, "MemoryWriteExclusive8[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return false;
+    }
+    bool MemoryWriteExclusive16(u64 vaddr, std::uint16_t value, std::uint16_t expected) override {
+        fprintf(stderr, "MemoryWriteExclusive16[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return false;
+    }
+    bool MemoryWriteExclusive32(u64 vaddr, std::uint32_t value, std::uint32_t expected) override {
+        fprintf(stderr, "MemoryWriteExclusive32[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return false;
+    }
+    bool MemoryWriteExclusive64(u64 vaddr, std::uint64_t value, std::uint64_t expected) override {
+        fprintf(stderr, "MemoryWriteExclusive64[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return false;
+    }
+    bool MemoryWriteExclusive128(u64 vaddr, Dynarmic::A64::Vector value, Dynarmic::A64::Vector expected) override {
+        fprintf(stderr, "MemoryWriteExclusive128[%s->%s:%d]: vaddr=%p\n", __FILE__, __func__, __LINE__, (void*)vaddr);
+        abort();
+        return false;
+    }
+
+    void InterpreterFallback(u64 pc, std::size_t num_instructions) override {
+        fprintf(stderr, "InterpreterFallback[%s->%s:%d]: pc=%p, num_instructions=%lu\n", __FILE__, __func__, __LINE__, (void*)pc, num_instructions);
+        abort();
+    }
+
+    void ExceptionRaised(u64 pc, Dynarmic::A64::Exception exception) override {
+        fprintf(stderr, "ExceptionRaised[%s->%s:%d]: pc=%p, exception=%d\n", __FILE__, __func__, __LINE__, (void*)pc, exception);
+        abort();
+    }
+
+    void CallSVC(u32 swi) override {
+        fprintf(stderr, "CallSVC[%s->%s:%d]: swi=%d\n", __FILE__, __func__, __LINE__, swi);
+        abort();
+    }
+
+    void AddTicks(u64 ticks) override {
+    }
+
+    u64 GetTicksRemaining() override {
+        return (u64) -1;
+    }
+
+    u64 GetCNTPCT() override {
+        return 0;
+    }
+};
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,6 +125,13 @@ JNIEXPORT jlong JNICALL Java_com_github_unidbg_arm_backend_dynarmic_Dynarmic_nat
   t_dynarmic dynarmic = (t_dynarmic) calloc(1, sizeof(struct dynarmic));
   dynarmic->is64Bit = is64Bit == JNI_TRUE;
   dynarmic->memory = kh_init(memory);
+  if(dynarmic->is64Bit) {
+    DynarmicCallbacks64 callbacks;
+    Dynarmic::A64::UserConfig config;
+    config.callbacks = &callbacks;
+    Dynarmic::A64::Jit jit{config};
+    dynarmic->jit64 = &jit;
+  }
   return (jlong) dynarmic;
 }
 
--- unidbg-api/src/main/native/dynarmic/dynarmic.h
+++ unidbg-api/src/main/native/dynarmic/dynarmic.h
@@ -1,3 +1,9 @@
+#include <dynarmic/A32/a32.h>
+#include <dynarmic/A32/config.h>
+
+#include <dynarmic/A64/a64.h>
+#include <dynarmic/A64/config.h>
+
 #include "khash.h"
 #include "com_github_unidbg_arm_backend_dynarmic_Dynarmic.h"
 
@@ -14,4 +20,5 @@ KHASH_MAP_INIT_INT64(memory, t_memory_page)
 typedef struct dynarmic {
   bool is64Bit;
   khash_t(memory) *memory;
+  Dynarmic::A64::Jit *jit64;
 } *t_dynarmic;
