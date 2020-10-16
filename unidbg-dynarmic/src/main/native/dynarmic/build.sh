7a84b5b4cdc7d4d0b5a72467d6df498a9a7266ef
android <android@gmail.com>
2020-10-17 00:43:47 +0800

    Update dynarmic build.sh

--- unidbg-dynarmic/src/main/native/dynarmic/build.sh
+++ unidbg-dynarmic/src/main/native/dynarmic/build.sh
@@ -2,7 +2,7 @@ JAVA_INC="$(realpath "$JAVA_HOME"/include)"
 JAVA_PLATFORM_INC="$(dirname "$(find "$JAVA_INC" -name jni_md.h)")"
 
 "$(/usr/libexec/java_home -v 1.8)"/bin/javah -cp ../../../../../unidbg-api/target/classes com.github.unidbg.arm.backend.dynarmic.Dynarmic && \
-  xcrun -sdk macosx clang++ -m64 -o libdynarmic.dylib -shared -std=c++17 \
+  xcrun -sdk macosx clang++ -m64 -o libdynarmic.dylib -shared -mmacosx-version-min=10.9 -std=c++17 \
   -I ~/git/dynarmic/include -I ~/git/dynarmic/externals/fmt/include dynarmic.cpp arm_dynarmic_cp15.cpp \
   -I "$JAVA_INC" -I "$JAVA_PLATFORM_INC" \
   ~/git/dynarmic/build/src/libdynarmic.a \
