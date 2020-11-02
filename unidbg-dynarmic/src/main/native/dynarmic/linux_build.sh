66f35ebedc599617b52cf7af1e0079e9f074c686
Banny <zhkl0228@gmail.com>
2020-11-02 23:16:22 +0800

    Add linux_build.sh

---
+++ unidbg-dynarmic/src/main/native/dynarmic/linux_build.sh
@@ -0,0 +1,9 @@
+JAVA_INC="$(realpath "$JAVA_HOME"/include)"
+JAVA_PLATFORM_INC="$(dirname "$(find "$JAVA_INC" -name jni_md.h)")"
+
+c++ -m64 -o libdynarmic.so -shared -fPIC -std=c++17 -O2 \
+  -I ~/git/dynarmic/include -I ~/git/dynarmic/externals/fmt/include dynarmic.cpp arm_dynarmic_cp15.cpp \
+  -I "$JAVA_INC" -I "$JAVA_PLATFORM_INC" \
+  ~/git/dynarmic/build/src/libdynarmic.a \
+  ~/git/dynarmic/build/externals/fmt/libfmt.a && \
+  mv libdynarmic.so ../../resources/natives/linux_64/
