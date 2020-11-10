2fa8d830d6202b5489866762837f3c7a2e1bb68a
Banny <zhkl0228@gmail.com>
2020-11-10 14:50:17 +0800

    Add kanxue test2

---
+++ unidbg-android/src/test/java/com/kanxue/test2/MainActivity.java
@@ -0,0 +1,66 @@
+package com.kanxue.test2;
+
+import com.github.unidbg.AndroidEmulator;
+import com.github.unidbg.LibraryResolver;
+import com.github.unidbg.arm.backend.dynarmic.DynarmicLoader;
+import com.github.unidbg.linux.android.AndroidARMEmulator;
+import com.github.unidbg.linux.android.AndroidResolver;
+import com.github.unidbg.linux.android.dvm.DalvikModule;
+import com.github.unidbg.linux.android.dvm.DvmObject;
+import com.github.unidbg.linux.android.dvm.VM;
+import com.github.unidbg.linux.android.dvm.jni.ProxyDvmObject;
+import com.github.unidbg.memory.Memory;
+
+import java.io.File;
+
+/**
+ * https://bbs.pediy.com/thread-263345.htm
+ */
+public class MainActivity {
+
+    public static void main(String[] args) {
+        new MainActivity().crack();
+    }
+
+    private final AndroidEmulator emulator;
+    private final VM vm;
+
+    private MainActivity() {
+        emulator = new AndroidARMEmulator();
+        Memory memory = emulator.getMemory();
+        LibraryResolver resolver = new AndroidResolver(23);
+        memory.setLibraryResolver(resolver);
+
+        vm = emulator.createDalvikVM(null);
+        DalvikModule dm = vm.loadLibrary(new File("unidbg-android/src/test/resources/example_binaries/armeabi-v7a/libnative-lib.so"), false);
+        dm.callJNI_OnLoad(emulator);
+    }
+
+    private static final char[] LETTERS = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+
+    static {
+        DynarmicLoader.useDynarmic();
+    }
+
+    private void crack() {
+        DvmObject<?> obj = ProxyDvmObject.createObject(vm, this);
+        long start = System.currentTimeMillis();
+        for (char a : LETTERS) {
+            for (char b : LETTERS) {
+                for (char c : LETTERS) {
+                    String str = "" + a + b + c;
+                    boolean success = obj.callJniMethodInt(emulator, "jnitest(Ljava/lang/String;)Z", str) == VM.JNI_TRUE;
+                    if (success) {
+                        System.out.println("Found: " + str + ", off=" + (System.currentTimeMillis() - start) + "ms");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
