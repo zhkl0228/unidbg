d429ee35b47e034901ea60e612a60d9c6be49b7a
zhkl0228 <zhkl0228@gmail.com>
2021-01-06 03:14:39 +0800

    Build libhypervisor.dylib

--- unidbg-hypervisor/src/main/native/hypervisor/hypervisor.cpp
+++ unidbg-hypervisor/src/main/native/hypervisor/hypervisor.cpp
@@ -1,1 +1,30 @@
 #include "com_github_unidbg_arm_backend_hypervisor_Hypervisor.h"
+
+#include <assert.h>
+
+#include <Hypervisor/Hypervisor.h>
+
+// Diagnostics
+#define HYP_ASSERT_SUCCESS(ret) assert((hv_return_t) (ret) == HV_SUCCESS)
+
+/*
+ * Class:     com_github_unidbg_arm_backend_hypervisor_Hypervisor
+ * Method:    setHypervisorCallback
+ * Signature: (JLcom/github/unidbg/arm/backend/hypervisor/HypervisorCallback;)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_unidbg_arm_backend_hypervisor_Hypervisor_setHypervisorCallback
+  (JNIEnv *env, jclass clazz, jlong handle, jobject callback) {
+  return 0;
+}
+
+/*
+ * Class:     com_github_unidbg_arm_backend_hypervisor_Hypervisor
+ * Method:    createVM
+ * Signature: (Z)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_unidbg_arm_backend_hypervisor_Hypervisor_createVM
+  (JNIEnv *env, jclass clazz, jboolean is64Bit) {
+  // Create the VM
+  HYP_ASSERT_SUCCESS(hv_vm_create(NULL));
+  return 0;
+}
--- unidbg-hypervisor/src/main/native/hypervisor/osx_build.sh
+++ unidbg-hypervisor/src/main/native/hypervisor/osx_build.sh
@@ -2,8 +2,8 @@ JAVA_INC="$(realpath "$JAVA_HOME"/include)"
 JAVA_PLATFORM_INC="$(dirname "$(find "$JAVA_INC" -name jni_md.h)")"
 
 "$(/usr/libexec/java_home -v 1.8)"/bin/javah -cp ../../../../../unidbg-api/target/classes com.github.unidbg.arm.backend.hypervisor.Hypervisor && \
-  xcrun -sdk macosx clang++ -m64 -o libhypervisor.dylib -shared -std=c++17 -O2 -mmacosx-version-min=10.9 \
+  xcrun -sdk macosx clang++ -m64 -o libhypervisor.dylib -shared -std=c++17 -O2 -mmacosx-version-min=11.0 \
   -framework Hypervisor hypervisor.cpp \
   -I "$JAVA_INC" -I "$JAVA_PLATFORM_INC" && \
-  codesign --entitlements hypervisor.entitlements -s - libhypervisor.dylib && \
+#  codesign --entitlements hypervisor.entitlements --force -s - "$JAVA_HOME"/bin/java && \
   mv libhypervisor.dylib ../../resources/natives/osx_arm64
