5c3487d47ff31c79c5593c3649eb1022cb96be68
Banny <zhkl0228@gmail.com>
2021-02-11 15:47:12 +0800

    kvm: continue.

--- backend/kvm/src/main/native/kvm.c
+++ backend/kvm/src/main/native/kvm.c
@@ -635,7 +635,7 @@ JNIEXPORT jint JNICALL Java_com_github_unidbg_arm_backend_kvm_Kvm_emu_1start
 
   if(kvm->is64Bit) {
     int cpsr = PSR_D_BIT | PSR_A_BIT | PSR_I_BIT | PSR_F_BIT | PSR_MODE_EL0t;
-    printf("emu_start cpsr=0x%x\n", cpsr);
+    printf("emu_start cpsr=0x%x, pc=0x%lx\n", cpsr, pc);
     HYP_ASSERT_SUCCESS(hv_vcpu_set_reg(cpu, HV_REG_CPSR, cpsr));
     HYP_ASSERT_SUCCESS(hv_vcpu_set_reg(cpu, HV_REG_PC, pc));
   } else {
