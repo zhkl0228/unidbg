6aa6ce47d5c6fa2bfb2f45edef2fd8302e37d76d
Banny <zhkl0228@gmail.com>
2021-02-11 15:20:57 +0800

    kvm: continue.

--- backend/kvm/src/main/java/com/github/unidbg/arm/backend/kvm/KvmBackend64.java
+++ backend/kvm/src/main/java/com/github/unidbg/arm/backend/kvm/KvmBackend64.java
@@ -46,6 +46,37 @@ public class KvmBackend64 extends KvmBackend {
     public void reg_write(int regId, Number value) throws BackendException {
         try {
             switch (regId) {
+                case Arm64Const.UC_ARM64_REG_X0:
+                case Arm64Const.UC_ARM64_REG_X1:
+                case Arm64Const.UC_ARM64_REG_X2:
+                case Arm64Const.UC_ARM64_REG_X3:
+                case Arm64Const.UC_ARM64_REG_X4:
+                case Arm64Const.UC_ARM64_REG_X5:
+                case Arm64Const.UC_ARM64_REG_X6:
+                case Arm64Const.UC_ARM64_REG_X7:
+                case Arm64Const.UC_ARM64_REG_X8:
+                case Arm64Const.UC_ARM64_REG_X9:
+                case Arm64Const.UC_ARM64_REG_X10:
+                case Arm64Const.UC_ARM64_REG_X11:
+                case Arm64Const.UC_ARM64_REG_X12:
+                case Arm64Const.UC_ARM64_REG_X13:
+                case Arm64Const.UC_ARM64_REG_X14:
+                case Arm64Const.UC_ARM64_REG_X15:
+                case Arm64Const.UC_ARM64_REG_X16:
+                case Arm64Const.UC_ARM64_REG_X17:
+                case Arm64Const.UC_ARM64_REG_X18:
+                case Arm64Const.UC_ARM64_REG_X19:
+                case Arm64Const.UC_ARM64_REG_X20:
+                case Arm64Const.UC_ARM64_REG_X21:
+                case Arm64Const.UC_ARM64_REG_X22:
+                case Arm64Const.UC_ARM64_REG_X23:
+                case Arm64Const.UC_ARM64_REG_X24:
+                case Arm64Const.UC_ARM64_REG_X25:
+                case Arm64Const.UC_ARM64_REG_X26:
+                case Arm64Const.UC_ARM64_REG_X27:
+                case Arm64Const.UC_ARM64_REG_X28:
+                    kvm.reg_write64(regId - Arm64Const.UC_ARM64_REG_X0, value.longValue());
+                    break;
                 case Arm64Const.UC_ARM64_REG_CPACR_EL1:
                     kvm.reg_set_cpacr_el1(value.longValue());
                     break;
