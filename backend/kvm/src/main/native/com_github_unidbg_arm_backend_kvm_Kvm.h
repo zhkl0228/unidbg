52dcc68d618dbcf7f7ccf32da9013ad250946e65
Banny <zhkl0228@gmail.com>
2021-02-11 07:18:33 +0000

    kvm: continue.

--- backend/kvm/src/main/native/com_github_unidbg_arm_backend_kvm_Kvm.h
+++ backend/kvm/src/main/native/com_github_unidbg_arm_backend_kvm_Kvm.h
@@ -73,6 +73,14 @@ JNIEXPORT jint JNICALL Java_com_github_unidbg_arm_backend_kvm_Kvm_reg_1set_1sp64
 
 /*
  * Class:     com_github_unidbg_arm_backend_kvm_Kvm
+ * Method:    reg_read_sp64
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_unidbg_arm_backend_kvm_Kvm_reg_1read_1sp64
+  (JNIEnv *, jclass, jlong);
+
+/*
+ * Class:     com_github_unidbg_arm_backend_kvm_Kvm
  * Method:    reg_set_tpidr_el0
  * Signature: (JJ)I
  */
