dd8cc88476279f3ba9a70bb33a2007b2eac9e4d5
Banny <zhkl0228@gmail.com>
2021-02-11 12:29:51 +0800

    kvm: continue.

--- backend/kvm/src/main/native/kvm.c
+++ backend/kvm/src/main/native/kvm.c
@@ -16,12 +16,27 @@ typedef struct kvm_cpu {
   struct kvm_run *run;
 } *t_kvm_cpu;
 
+static int check_one_reg(uint64_t reg, int ret) {
+  switch(ret) {
+    case ENOENT:
+      fprintf("no such register: reg=0x%llx\n", reg);
+      break;
+    case EINVAL:
+      fprintf("invalid register ID, or no such register: reg=0x%llx\n", reg);
+      break;
+    case EPERM:
+      fprintf("(arm64) register access not allowed before vcpu finalization: reg=0x%llx\n", reg);
+      break;
+  }
+  return ret;
+}
+
 hv_return_t hv_vcpu_get_reg(hv_vcpu_t vcpu, hv_reg_t reg, uint64_t *value) {
     struct kvm_one_reg reg_req = {
         .id = reg,
         .addr = (uint64_t)value,
     };
-    if (ioctl(vcpu->fd, KVM_GET_ONE_REG, &reg_req) < 0) {
+    if (check_one_reg(reg, ioctl(vcpu->fd, KVM_GET_ONE_REG, &reg_req)) < 0) {
         return -1;
     }
     return HV_SUCCESS;
@@ -32,7 +47,7 @@ hv_return_t hv_vcpu_set_reg(hv_vcpu_t vcpu, hv_reg_t reg, uint64_t value) {
         .id = reg,
         .addr = (uint64_t)&value,
     };
-    if (ioctl(vcpu->fd, KVM_SET_ONE_REG, &reg_req) < 0) {
+    if (check_one_reg(reg, ioctl(vcpu->fd, KVM_SET_ONE_REG, &reg_req)) < 0) {
         return -1;
     }
     return HV_SUCCESS;
@@ -43,7 +58,7 @@ hv_return_t hv_vcpu_get_sys_reg(hv_vcpu_t vcpu, hv_sys_reg_t reg, uint64_t *valu
         .id = reg,
         .addr = (uint64_t)value,
     };
-    if (ioctl(vcpu->fd, KVM_GET_ONE_REG, &reg_req) < 0) {
+    if (check_one_reg(reg, ioctl(vcpu->fd, KVM_GET_ONE_REG, &reg_req)) < 0) {
         return -1;
     }
     return HV_SUCCESS;
@@ -54,7 +69,7 @@ hv_return_t hv_vcpu_set_sys_reg(hv_vcpu_t vcpu, hv_sys_reg_t reg, uint64_t value
         .id = reg,
         .addr = (uint64_t)&value,
     };
-    if (ioctl(vcpu->fd, KVM_SET_ONE_REG, &reg_req) < 0) {
+    if (check_one_reg(reg, ioctl(vcpu->fd, KVM_SET_ONE_REG, &reg_req)) < 0) {
         return -1;
     }
     return HV_SUCCESS;
@@ -65,7 +80,7 @@ hv_return_t hv_vcpu_get_simd_fp_reg(hv_vcpu_t vcpu, hv_simd_fp_reg_t reg, hv_sim
         .id = reg,
         .addr = (uint64_t)value,
     };
-    if (ioctl(vcpu->fd, KVM_GET_ONE_REG, &reg_req) < 0) {
+    if (check_one_reg(reg, ioctl(vcpu->fd, KVM_GET_ONE_REG, &reg_req)) < 0) {
         return -1;
     }
     return HV_SUCCESS;
@@ -76,7 +91,7 @@ hv_return_t hv_vcpu_set_simd_fp_reg(hv_vcpu_t vcpu, hv_simd_fp_reg_t reg, hv_sim
         .id = reg,
         .addr = (uint64_t)&value,
     };
-    if (ioctl(vcpu->fd, KVM_SET_ONE_REG, &reg_req) < 0) {
+    if (check_one_reg(reg, ioctl(vcpu->fd, KVM_SET_ONE_REG, &reg_req)) < 0) {
         return -1;
     }
     return HV_SUCCESS;
