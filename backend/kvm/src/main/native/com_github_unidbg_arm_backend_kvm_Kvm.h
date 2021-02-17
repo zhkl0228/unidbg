a1b3acd771f1132397773f5a79759e013de1acc6
Banny <zhkl0228@gmail.com>
2021-02-18 03:50:13 +0800

    kvm: continue.

--- backend/kvm/src/main/native/com_github_unidbg_arm_backend_kvm_Kvm.h
+++ backend/kvm/src/main/native/com_github_unidbg_arm_backend_kvm_Kvm.h
@@ -58,10 +58,10 @@ JNIEXPORT jlong JNICALL Java_com_github_unidbg_arm_backend_kvm_Kvm_set_1user_1me
 /*
  * Class:     com_github_unidbg_arm_backend_kvm_Kvm
  * Method:    remove_user_memory_region
- * Signature: (JIJJJ)I
+ * Signature: (JIJJJJ)I
  */
 JNIEXPORT jint JNICALL Java_com_github_unidbg_arm_backend_kvm_Kvm_remove_1user_1memory_1region
-  (JNIEnv *, jclass, jlong, jint, jlong, jlong, jlong);
+  (JNIEnv *, jclass, jlong, jint, jlong, jlong, jlong, jlong);
 
 /*
  * Class:     com_github_unidbg_arm_backend_kvm_Kvm
