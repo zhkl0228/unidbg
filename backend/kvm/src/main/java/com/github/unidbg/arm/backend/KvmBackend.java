6e6ba6e93eee6df4c2828f01ad879df4aa089824
Banny <zhkl0228@gmail.com>
2021-02-17 23:20:57 +0800

    kvm: continue.

--- backend/kvm/src/main/java/com/github/unidbg/arm/backend/KvmBackend.java
+++ backend/kvm/src/main/java/com/github/unidbg/arm/backend/KvmBackend.java
@@ -147,11 +147,14 @@ public abstract class KvmBackend extends FastBackend implements Backend, KvmCall
     public synchronized void emu_start(long begin, long until, long timeout, long count) throws BackendException {
         if (log.isDebugEnabled()) {
             log.debug("emu_start begin=0x" + Long.toHexString(begin) + ", until=0x" + Long.toHexString(until) + ", timeout=" + timeout + ", count=" + count);
-
-            if (begin == 0x40a3aaac) {
-                emulator.attach().addBreakPoint(0x40a3aaac);
-            }
         }
+
+        if (begin == 0x40a3aaac) {
+            long addr = 0x40a3aaac;
+            emulator.attach().addBreakPoint(addr);
+            emulator.attach().disassembleBlock(emulator, addr, false);
+        }
+
         this.until = until + 4;
         try {
             kvm.emu_start(begin);
