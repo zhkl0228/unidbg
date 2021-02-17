141379df942ad494fb79896c1c410c47bc9d69aa
Banny <zhkl0228@gmail.com>
2021-02-18 00:35:10 +0800

    kvm: continue.

--- backend/kvm/src/main/java/com/github/unidbg/arm/backend/KvmBackend.java
+++ backend/kvm/src/main/java/com/github/unidbg/arm/backend/KvmBackend.java
@@ -129,7 +129,7 @@ public abstract class KvmBackend extends FastBackend implements Backend, KvmCall
             }
             if (address < 0x40a3aa98 && address + bytes.length >= 0x40a3aa98 + 4) {
                 long addr = 0x40a3aa98;
-                emulator.attach().disassembleBlock(emulator, addr, false);
+                emulator.attach().disassembleBlock(emulator, addr - 4, false);
             }
         } catch (KvmException e) {
             throw new BackendException(e);
