081488813ee669fe2ba93869384858aab16f01c5
Banny <zhkl0228@gmail.com>
2021-02-19 14:25:17 +0800

    kvm: continue AArch32.

--- backend/kvm/src/main/native/kvm.c
+++ backend/kvm/src/main/native/kvm.c
@@ -513,6 +513,19 @@ JNIEXPORT jint JNICALL Java_com_github_unidbg_arm_backend_kvm_Kvm_reg_1set_1cpac
 
 /*
  * Class:     com_github_unidbg_arm_backend_kvm_Kvm
+ * Method:    reg_set_fpexc
+ * Signature: (JJ)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_unidbg_arm_backend_kvm_Kvm_reg_1set_1fpexc
+  (JNIEnv *env, jclass clazz, jlong handle, jlong value) {
+  t_kvm kvm = (t_kvm) handle;
+  t_kvm_cpu cpu = kvm->cpu;
+  HYP_ASSERT_SUCCESS(hv_vcpu_set_sys_reg(cpu, HV_SYS_REG_FPEXC32_EL2, value));
+  return 0;
+}
+
+/*
+ * Class:     com_github_unidbg_arm_backend_kvm_Kvm
  * Method:    reg_set_sp64
  * Signature: (JJ)I
  */
