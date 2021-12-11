c090dc3c89251b744cc7efea85dc89c4f07d28c2
Banny <zhkl0228@gmail.com>
2021-12-12 00:21:47 +0800

    backend/unicorn2: Add apple arm64 binary.

--- backend/unicorn2/src/main/native/osx_build_arm64.sh
+++ backend/unicorn2/src/main/native/osx_build_arm64.sh
@@ -1,11 +1,13 @@
+# UNICORN_ARCHS="arm aarch64" UNICORN_STATIC=yes ./make.sh
+
 JAVA_INC="$(realpath "$JAVA_HOME"/include)"
 JAVA_PLATFORM_INC="$(dirname "$(find "$JAVA_INC" -name jni_md.h)")"
 
-UNICORN_HOME=~/Downloads/unicorn
+UNICORN_HOME=~/git/unicorn
 
 "$(/usr/libexec/java_home -v 1.8)"/bin/javah -cp ../../../target/classes com.github.unidbg.arm.backend.unicorn.Unicorn && \
   xcrun -sdk macosx clang -m64 -o libunicorn.dylib -shared -O3 -DNDEBUG -arch arm64 -mmacosx-version-min=11.6 \
   -I $UNICORN_HOME/include unicorn.c sample_arm.c sample_arm64.c \
-  -I "$JAVA_INC" -I "$JAVA_PLATFORM_INC" -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wall -fPIC -Wno-missing-braces \
-  $UNICORN_HOME/build/libaarch64-softmmu.a $UNICORN_HOME/build/libarm-softmmu.a $UNICORN_HOME/build/libunicorn-common.a $UNICORN_HOME/build/libunicorn.a &&
+  -I "$JAVA_INC" -I "$JAVA_PLATFORM_INC" -fPIC \
+  $UNICORN_HOME/libunicorn.a &&
   mv libunicorn.dylib ../resources/natives/osx_arm64
