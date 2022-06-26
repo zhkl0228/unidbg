fffb959439b8fa3d50564a1aa9374d67ba58357a
Pr0214 <44285766+Pr0214@users.noreply.github.com>
2022-06-26 16:50:20 +0800

    Update FileListener.java

--- unidbg-api/src/main/java/com/github/unidbg/unix/FileListener.java
+++ unidbg-api/src/main/java/com/github/unidbg/unix/FileListener.java
@@ -2,13 +2,14 @@ package com.github.unidbg.unix;
 
 import com.github.unidbg.Emulator;
 import com.github.unidbg.file.FileIO;
+import com.sun.jna.Pointer;
 
 public interface FileListener {
 
     void onOpenSuccess(Emulator<?> emulator, String pathname, FileIO io);
 
-    void onRead(Emulator<?> emulator, String pathname, byte[] bytes);
-    void onWrite(Emulator<?> emulator, String pathname, byte[] bytes);
+    void onRead(Emulator<?> emulator, String pathname, Pointer buffer, byte[] bytes);
+    void onWrite(Emulator<?> emulator, String pathname, Pointer buffer, byte[] bytes);
 
     void onClose(Emulator<?> emulator, FileIO io);
 
