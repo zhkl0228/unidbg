13c14e6ff61b4ba8563d1dc5b6bd815145783c9e
x-cold <lxstart@outlook.com>
2023-06-10 23:08:55 +0800

    chore: update dynarmic backend linux build script

--- backend/dynarmic/src/main/native/dynarmic/linux_build.sh
+++ backend/dynarmic/src/main/native/dynarmic/linux_build.sh
@@ -1,9 +1,15 @@
 JAVA_INC="$JAVA_HOME"/include
 JAVA_PLATFORM_INC="$(dirname "$(find "$JAVA_INC" -name jni_md.h)")"
 
-c++ -m64 -o libdynarmic.so -shared -fPIC -std=c++17 -O2 \
-  -I ~/git/dynarmic/include -I ~/git/dynarmic/externals/fmt/include dynarmic.cpp arm_dynarmic_cp15.cpp \
-  -I "$JAVA_INC" -I "$JAVA_PLATFORM_INC" \
-  ~/git/dynarmic/build/src/libdynarmic.a \
-  ~/git/dynarmic/build/externals/fmt/libfmt.a && \
-  mv libdynarmic.so ../../resources/natives/linux_64/
+# Docker path
+DYNARMIC_HOME=/github/dynarmic
+
+c++  -m64 -o libdynarmic.so -shared -fPIC -std=c++17 -O2 \
+  -I $DYNARMIC_HOME/src -I $DYNARMIC_HOME/externals/fmt/include dynarmic.cpp arm_dynarmic_cp15.cpp \
+  -I "$JAVA_INC" -I "$JAVA_PLATFORM_INC" -DDYNARMIC_MASTER \
+  $DYNARMIC_HOME/build/src/dynarmic/libdynarmic.a \
+  $DYNARMIC_HOME/build/externals/zydis/libZydis.a \
+  $DYNARMIC_HOME/build/externals/mcl/src/libmcl.a \
+  $DYNARMIC_HOME/build/externals/fmt/libfmt.a && \
+
+mv libdynarmic.so ../../resources/natives/linux_64/
