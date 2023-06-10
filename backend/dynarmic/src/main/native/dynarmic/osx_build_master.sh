adc945f76f6d0c6f2c66f171bed07d19716b3b5e
x-cold <lxstart@outlook.com>
2023-06-10 23:08:39 +0800

    chore: update dynarmic backend osx build script

--- backend/dynarmic/src/main/native/dynarmic/osx_build_master.sh
+++ backend/dynarmic/src/main/native/dynarmic/osx_build_master.sh
@@ -1,7 +1,8 @@
-JAVA_INC="$(realpath "$JAVA_HOME"/include)"
+# JAVA_HOME=/Library/Java/JavaVirtualMachines/openlogic-openjdk-8.jdk/Contents/Home
+JAVA_INC="$JAVA_HOME"/include
 JAVA_PLATFORM_INC="$(dirname "$(find "$JAVA_INC" -name jni_md.h)")"
 
-DYNARMIC_HOME=~/Downloads/dynarmic
+DYNARMIC_HOME=~/git/dynarmic
 
 "$(/usr/libexec/java_home -v 1.8)"/bin/javah -cp ../../../../target/classes com.github.unidbg.arm.backend.dynarmic.Dynarmic && \
   xcrun -sdk macosx clang++ -m64 -o libdynarmic.dylib -shared -std=c++17 -O2 -mmacosx-version-min=10.15 \
@@ -9,5 +10,7 @@ DYNARMIC_HOME=~/Downloads/dynarmic
   -I "$JAVA_INC" -I "$JAVA_PLATFORM_INC" -DDYNARMIC_MASTER \
   $DYNARMIC_HOME/build/src/dynarmic/libdynarmic.a \
   $DYNARMIC_HOME/build/externals/zydis/libZydis.a \
-  $DYNARMIC_HOME/build/externals/fmt/libfmt.a && \
-  mv libdynarmic.dylib ../../resources/natives/osx_64/
+  $DYNARMIC_HOME/build/externals/mcl/src/libmcl.a \
+  $DYNARMIC_HOME/build/externals/fmt/libfmt.a
+
+mv libdynarmic.dylib ../../resources/natives/osx_64/
